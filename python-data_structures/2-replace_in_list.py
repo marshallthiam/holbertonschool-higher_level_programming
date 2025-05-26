fabd46003f000600341ab101d5b33a64c541f54f
marshallthiam <marshallthiam@holbertonschool-higher_level_programming.com>
2025-05-26 10:49:18 +0000

    Replace element

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
-# 10-divisible_by_2.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
-def divisible_by_2(my_list=[]):
-    """Find all multiples of 2 in a list."""
-    multiples = []
-    for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
-            multiples.append(True)
-        else:
-            multiples.append(False)
-
-    return (multiples)
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
