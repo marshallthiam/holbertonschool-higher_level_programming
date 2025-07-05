624ecee3c31a84f49a09462c525f32e71399627f
marshallthiam <marshallthiam@holbertonstudents.com>
2025-07-05 19:07:33 +0000

    Only Comedy

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- Com
+SELECT tv_shows.title
+FROM tv_shows, tv_show_genres, tv_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+  AND tv_genres.id = tv_show_genres.genre_id
+  AND tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title;
